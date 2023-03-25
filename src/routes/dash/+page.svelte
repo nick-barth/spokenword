f8d7958ebde6780971b499d2401239cb34600d53
nick-barth <barth.nicholas@gmail.com>
2023-03-25 17:29:30 +0100

    auth up and running

---
+++ src/routes/dash/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { onMount } from 'svelte';
+	import db from '../db';
+	import { goto } from '$app/navigation';
+	let bookmarks = [];
+	onMount(async () => {
+		bookmarks = await db.bookmarks.all();
+	});
+	async function handleInsertBookmark() {
+		const bookmark = await db.bookmarks.create({
+			url: 'https://blog.hireproof.io/big-twitch-energy-how-live-streaming-made-me-a-better-developer-ae61e09c8c48'
+		});
+	}
+</script>
+
+<div>
+	I'm protected route
+	<button on:click={handleInsertBookmark}>Insert bookmark</button>
+
+	<h2>Bookmarks</h2>
+	<div>
+		{#each bookmarks as bookmark}
+			<li>
+				{bookmark.url}
+			</li>
+		{/each}
+	</div>
+</div>
---
+++ src/routes/db.ts
@@ -0,0 +1,59 @@
+import { createClient } from '@supabase/supabase-js';
+import { writable } from 'svelte/store';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY, PUBLIC_BASE_URL } from '$env/static/public';
+
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY);
+
+const userStore = writable();
+
+supabase.auth.getSession().then(({ data }) => {
+	userStore.set(data.session?.user);
+});
+
+supabase.auth.onAuthStateChange((event, session) => {
+	if (event == 'SIGNED_IN' && session) {
+		userStore.set(session.user);
+	} else if (event == 'SIGNED_OUT') {
+		userStore.set(null);
+	}
+});
+
+export default {
+	get user() {
+		return userStore;
+	},
+	signUp(email: string, password: string) {
+		return supabase.auth.signUp({
+			email: email,
+			password: password
+		});
+	},
+	async signIn(email: string, password: string) {
+		const { data, error } = await supabase.auth.signInWithPassword({
+			email,
+			password
+		});
+	},
+	signOut() {
+		return supabase.auth.signOut();
+	},
+	bookmarks: {
+		async all() {
+			const { data } = await supabase.from('bookmarks').select('*');
+
+			return data;
+		},
+
+		async create(bookmark) {
+			const {
+				data: { user }
+			} = await supabase.auth.getUser();
+			const { data, error } = await supabase.from('bookmarks').insert({
+				...bookmark,
+				user_id: user.id
+			});
+
+			return data;
+		}
+	}
+};
